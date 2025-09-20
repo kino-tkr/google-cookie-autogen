318a69a67c087ba6d52e22988b91c36a97820761
kino-tkr <v6111145@gmail.com>
2025-09-21 01:19:00 +0900

    no captcha

--- script.py
+++ script.py
@@ -2,6 +2,26 @@ from DrissionPage import ChromiumPage, ChromiumOptions
 from pyvirtualdisplay import Display
 import json
 import time
+import sys
+import random
+import traceback
+import os
+
+tinyproxy_template = """Port 8888
+Listen 127.0.0.1
+Timeout 600
+Allow 127.0.0.1
+Upstream http {}
+LogLevel Info"""
+
+def random_proxy(proxy):
+    try:
+        os.mkdir("/etc/tinyproxy")
+    except:
+        pass
+    proxy = proxy.replace(":1080", f":{random.randint(1080, 1380)}")
+    open("/etc/tinyproxy/tinyproxy.conf", "w", encoding="utf_8").write(tinyproxy_template.format(proxy)
+    os.system("tinyproxy")
 
 def main():
     display = Display(visible=0, size=(1920, 1080))
@@ -10,6 +30,11 @@ def main():
     options = ChromiumOptions()
     options.set_argument('--no-sandbox')
     options.set_argument('--disable-dev-shm-usage')
+    try:
+        random_proxy(sys.argv[1])
+        options.set_argument('--proxy-server="http://localhost:8888"')
+    except:
+        pass
     #options.set_argument('--disable-blink-features=AutomationControlled')
     options.headless(False)
 
