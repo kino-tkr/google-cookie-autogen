59cd3b739a42d0d64377fc9004ae17c33e6a2566
kino-tkr <v6111145@gmail.com>
2025-09-21 01:46:42 +0900

    rage

--- script.py
+++ script.py
@@ -2,31 +2,9 @@ from DrissionPage import ChromiumPage, ChromiumOptions
 from pyvirtualdisplay import Display
 import json
 import time
-import sys
-import random
 import traceback
 import os
 import requests
-
-tinyproxy_template = """Port 8888
-Listen 127.0.0.1
-Timeout 600
-Allow 127.0.0.1
-Upstream http {}
-LogLevel Info"""
-
-def random_proxy(proxy):
-    try:
-        os.mkdir("/etc/tinyproxy")
-    except:
-        pass
-    proxy = proxy.replace(":1080", f":{random.randint(1080, 1380)}")
-    open("/etc/tinyproxy/tinyproxy.conf", "w", encoding="utf_8").write(tinyproxy_template.format(proxy))
-    os.system("tinyproxy")
-    try:
-        print("proxy", requests.get("https://api.ipify.org", proxies={"http": "http://"+proxy, "https": "http://"+proxy}).text)
-    except:
-        print("proxy connect failure")
 
 def main():
     display = Display(visible=0, size=(1920, 1080))
@@ -36,12 +14,10 @@ def main():
     options.set_argument('--no-sandbox')
     options.set_argument('--disable-dev-shm-usage')
     try:
-        random_proxy(sys.argv[1])
+        print("proxy", requests.get("https://api.ipify.org", proxies={"http": "http://localhost:8888", "https": "http://localhost:8888"}, timeout=3).text)
         options.set_argument('--proxy-server="http://localhost:8888"')
     except:
-        traceback.print_exc()
         print("proxyless", requests.get("https://api.ipify.org").text)
-        pass
     #options.set_argument('--disable-blink-features=AutomationControlled')
     options.headless(False)
 
