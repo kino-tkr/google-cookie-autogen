4fb72b4c748b1e8b795b44d1395ee3c57fd93786
kino-tkr <v6111145@gmail.com>
2025-09-21 01:26:23 +0900

    check ip

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ import sys
 import random
 import traceback
 import os
+import requests
 
 tinyproxy_template = """Port 8888
 Listen 127.0.0.1
@@ -22,6 +23,10 @@ def random_proxy(proxy):
     proxy = proxy.replace(":1080", f":{random.randint(1080, 1380)}")
     open("/etc/tinyproxy/tinyproxy.conf", "w", encoding="utf_8").write(tinyproxy_template.format(proxy))
     os.system("tinyproxy")
+    try:
+        print("proxy", requests.get("https://api.ipify.org", proxies={"http": "http://"+proxy, "https": "http://"+proxy}).text)
+    except:
+        print("proxy connect failure")
 
 def main():
     display = Display(visible=0, size=(1920, 1080))
@@ -34,6 +39,8 @@ def main():
         random_proxy(sys.argv[1])
         options.set_argument('--proxy-server="http://localhost:8888"')
     except:
+        traceback.print_exc()
+        print("proxyless", requests.get("https://api.ipify.org").text)
         pass
     #options.set_argument('--disable-blink-features=AutomationControlled')
     options.headless(False)
